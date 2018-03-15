8754637cee126882a4ac0f12b19a316d322c6bb5
paruthidotexe <paruthidotexe@gmail.com>
2018-03-15 12:20:07 +0530

    Webpage testing analyse the content

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def PonsPrint(printType, printVal):
 
 
 def GetRecipeDetails(urlpath):
-    PonsPrint (False, urlpath)
+    PonsPrint (False, urlpath + "\n")
     
     htmlFile = urlopen(urlpath)
 
@@ -34,7 +34,8 @@ def GetRecipeDetails(urlpath):
     soup = BeautifulSoup(htmlContent, "html.parser")
 
     ingredients = soup.find("div", class_="heading")
-    PonsPrint(False,ingredients.h2.text)
+    recipeName = ingredients.h2.text
+    PonsPrint(False,"Recipe : " + recipeName)
 
     recipeDetails = soup.find("div", class_="recipe-detail-rating-block")
     cuisine = recipeDetails.find("ul", class_="pre-tags")
@@ -43,25 +44,59 @@ def GetRecipeDetails(urlpath):
 
     ingredients = soup.find_all("div", class_="ingredients")
     PonsPrint(False, ingredients[0].h3.text)
-    for curingredients in ingredients:
-        if curingredients.ul:
-            ingredientsList = curingredients.ul.find_all("li")
-            if(ingredientsList):
-                for li in ingredientsList:
-                    PonsPrint(False,li.text)
+    curingredients = ingredients[0]
+    if curingredients.ul:
+        ingredientsList = curingredients.ul.find_all("li")
+        if(ingredientsList):
+            for li in ingredientsList:
+                PonsPrint(False,li.get_text().strip())
+    elif curingredients.ol:
+        ingredientsList = curingredients.ol.find_all("li")
+        if(ingredientsList):
+            for li in ingredientsList:
+                PonsPrint(False, li.get_text().strip())
+    elif curingredients:
+        ingredientsList = curingredients.find_all("div")
+        if(ingredientsList):
+            for li in ingredientsList:
+                PonsPrint(False, li.get_text().strip())
+        else:
+            ingredientsListNew = curingredients.find_all("p")
+            if(ingredientsListNew):
+                for li in ingredientsListNew:
+                    PonsPrint(False, li.get_text().strip())
 
     ingredients = soup.find_all("div", class_="ingredients")
     PonsPrint(False, "\n" + ingredients[1].h3.text)
     cookwareList = ingredients[1].find_all("a")
-    for curCookware in cookwareList:
-        PonsPrint(False,curCookware.attrs['href'])
+    if cookwareList:
+        for curCookware in cookwareList:
+            PonsPrint(False,curCookware.attrs['href'])
+    else:
+        cookwareListNew = ingredients[1].find_all("p")
+        for curCookware in cookwareListNew:
+            PonsPrint(False,curCookware.get_text().strip())
 
     #ingredients = soup.find_all("div", class_="recipe-steps")
     recipeSteps = soup.find_all("div", class_="step-detail")
     PonsPrint(False,"\nStep by step")
     recipeSteps = recipeSteps[0].find_all("p")
     for curRecipeStep in recipeSteps:
-        PonsPrint(False, curRecipeStep.text)
+        PonsPrint(False, curRecipeStep.text.strip())
+
+    recipeSteps = soup.find_all("div", class_="detail", id="chef-recips-title")
+    for curRecipeStep in recipeSteps:
+        PonsPrint(False, "\nChef : " + curRecipeStep.h4.get_text())
+
+    # recipeSteps = soup.find_all("div", id="divImage")
+    # for curRecipeStep in recipeSteps:
+    #     imgTags = curRecipeStep.find_all("img", id="imgRecipe")
+    #     for imgTag in imgTags:
+    #         relativePath = imgTag["src"]
+    #         fileExt = relativePath.rsplit(".")[-1:]
+    #         if fileExt[0]:
+    #             imgPath = urllib.parse.urljoin(urlpath, relativePath)
+    #             urllib.request.urlretrieve(imgPath, recipeName + "." + fileExt[0])
 
     PonsPrint (False, "\n----------------------------End of Recipe--------------------------------------\n")
 
@@ -70,10 +105,20 @@ if __name__ == '__main__':
     recipePaths = [
         'http://www.prestigesmartchef.com/recipe/view/italian-pasta-penne-allaarrabiata',
           "http://www.prestigesmartchef.com/recipe/view/chicken-tikka",
+          "http://www.prestigesmartchef.com/recipe/view/brownie-cake",
+          "http://www.prestigesmartchef.com/recipe/view/veg-hakka-noodle",
+          "http://www.prestigesmartchef.com/recipe/view/indian-cottage-cheese-with-fenugreek-paneer-methi",
+          "http://www.prestigesmartchef.com/recipe/view/dum-aloo",
           "http://www.prestigesmartchef.com/recipe/view/shammi-kebab",
-          "http://www.prestigesmartchef.com/recipe/view/hariyali-chicken-kadahi"
-          
+          "http://www.prestigesmartchef.com/recipe/view/hariyali-chicken-kadahi",
+          "http://www.prestigesmartchef.com/recipe/view/reshmi-paneer-seekh",
+          "http://www.prestigesmartchef.com/recipe/view/indian-cottage-cheese-with-fenugreek-paneer-methi",
+          "http://www.prestigesmartchef.com/recipe/view/paneer-tikka"          
     ]
+
+    # recipePaths = [
+    #       "http://www.prestigesmartchef.com/recipe/view/italian-pasta-penne-allaarrabiata",
+    # ]
     
     outputFilePath = "RecipeList.txt"
     outputFile = open(outputFilePath, "w")
